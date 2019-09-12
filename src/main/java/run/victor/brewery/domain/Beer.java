9fde3df27f1b449d31736ba9edf82188488f2381
Victor Wardi <victorcaetano@live.com>
2019-09-12 20:25:05 -0300

    Creating Beer Model Class

---
+++ src/main/java/run/victor/brewery/domain/Beer.java
@@ -0,0 +1,57 @@
+package run.victor.brewery.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Version;
+import java.math.BigDecimal;
+import java.sql.Timestamp;
+import java.util.UUID;
+
+import com.fasterxml.jackson.annotation.JsonTypeId;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.UpdateTimestamp;
+
+/**
+ * Created by Victor Wardi - @victorwardi on 9/12/2019
+ */
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+public class Beer {
+
+    @Id
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
+    @Column(length = 36, columnDefinition = "varchar", updatable = false, nullable = false)
+    private UUID id;
+
+    @Version
+    private Long version;
+
+    @CreationTimestamp
+    private Timestamp createdDate;
+
+    @UpdateTimestamp
+    private Timestamp modifiedDate;
+    private String name;
+    private String style;
+
+    @Column(unique = true)
+    private Long upc;
+    private BigDecimal price;
+
+    private Integer minOnHand;
+    private Integer quantityToBrew;
+}
